f0c3eb7490b02579696d5a58a7ed69e7ab060908
Marek Kadek <kadek.marek@gmail.com>
2015-09-03 08:18:02 +1200

    test test

--- cluster-tests/src/multi-jvm/scala/SomeTest.scala
+++ cluster-tests/src/multi-jvm/scala/SomeTest.scala
@@ -21,9 +21,69 @@ object SampleMultiJvmNode3 {
 
 // TODO
 
-import akka.remote.testkit.MultiNodeConfig
+import akka.remote.testkit._
+import org.scalatest._
+import org.scalatest.matchers._
+import akka.actor._
+import akka.testkit._
+
+/**
+ * Hooks up MultiNodeSpec with ScalaTest
+ */
+trait STMultiNodeSpec extends MultiNodeSpecCallbacks
+with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  override def beforeAll() = multiNodeSpecBeforeAll()
+
+  override def afterAll() = multiNodeSpecAfterAll()
+}
 
 object MultiNodeSampleConfig extends MultiNodeConfig {
   val node1 = role("node1")
   val node2 = role("node2")
+}
+
+
+class MultiNodeSampleSpecMultiJvmNode1 extends MultiNodeSample
+class MultiNodeSampleSpecMultiJvmNode2 extends MultiNodeSample
+
+object MultiNodeSample {
+  class Ponger extends Actor {
+    def receive = {
+      case "ping" => sender() ! "pong"
+    }
+  }
+}
+
+class MultiNodeSample extends MultiNodeSpec(MultiNodeSampleConfig)
+with STMultiNodeSpec with ImplicitSender {
+
+  import MultiNodeSampleConfig._
+  import MultiNodeSample._
+
+  def initialParticipants = roles.size
+
+  "A MultiNodeSample" must {
+
+    "wait for all nodes to enter a barrier" in {
+      enterBarrier("startup")
+    }
+
+    "send to and receive from a remote node" in {
+      runOn(node1) {
+        enterBarrier("deployed")
+        val ponger = system.actorSelection(node(node2) / "user" / "ponger")
+        ponger ! "ping"
+        import scala.concurrent.duration._
+        expectMsg(10.seconds, "pong")
+      }
+
+      runOn(node2) {
+        system.actorOf(Props[Ponger], "ponger")
+        enterBarrier("deployed")
+      }
+
+      enterBarrier("finished")
+    }
+  }
 }
